8f64e1ae3346c6b5832debf33546613e799e24fc
Ethan Neff <ethan.neff@eneff.com>
2025-05-29 23:46:04 -0700

    add button storybook

--- src/screens/OverviewScreen/OverviewScreen.tsx
+++ src/screens/OverviewScreen/OverviewScreen.tsx
@@ -300,6 +300,29 @@ const TypographyCard = () => {
   );
 };
 
+const ButtonCard = () => {
+  const { spacing } = useAppTheme();
+  return (
+    <Card>
+      <View gap={spacing.$12}>
+        <Button title="Full Width" onPress={() => {}} variant="outline" />
+        <View gap={spacing.$12} flexDirection="row" flexWrap="wrap" justifyContent="center">
+          <Button title="Primary" onPress={() => {}} variant="primary" />
+          <Button title="Secondary" onPress={() => {}} variant="secondary" />
+          <Button title="Destructive" onPress={() => {}} variant="destructive" />
+          <Button title="Outline" onPress={() => {}} variant="outline" />
+          <Button title="Ghost" onPress={() => {}} variant="ghost" />
+          <Button title="Link" onPress={() => {}} variant="link" />
+          <Button title="Disabled" onPress={() => {}} variant="primary" disabled />
+          <Button title="Loading" onPress={() => {}} variant="outline" loading />
+          <Button title="" onPress={() => {}} variant="outline" icon="logo-google" />
+          <Button title="With Icon" onPress={() => {}} variant="outline" icon="logo-google" />
+        </View>
+      </View>
+    </Card>
+  );
+};
+
 const OverviewScreen = () => {
   const { colors, spacing } = useAppTheme();
   return (
