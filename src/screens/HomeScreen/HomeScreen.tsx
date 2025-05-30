a8ad27d75f9beb61d824b5b5c2ebd16308344569
Ethan Neff <ethan.neff@eneff.com>
2025-05-29 23:45:36 -0700

    update buttons on homescreen

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,7 +1,6 @@
 import { useNavigation } from '@react-navigation/native';
 import { Alert } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-controller';
-
 import { Button } from '~/components/Button/Button';
 import { Card } from '~/components/Card/Card';
 import { Screen } from '~/components/Screen/Screen';
@@ -14,7 +13,7 @@ import { useAppTheme } from '~/theme/useAppTheme';
 const HomeScreen = () => {
   const { spacing } = useAppTheme();
   const navigation = useNavigation();
-  const login = useStoreAuth((state) => state.actions.login);
+
   const logout = useStoreAuth((state) => state.actions.logout);
   const user = useStoreAuth((state) => state.user);
   const toggleTheme = useStoreTheme((state) => state.actions.toggleTheme);
@@ -26,33 +25,51 @@ const HomeScreen = () => {
         <View padding={spacing.$12} gap={spacing.$12}>
           <Card>
             <Text title="Navigation" size="4xl" weight="light" />
-            <View alignSelf="center">
-              <Button title="Debug" onPress={() => navigation.navigate('Debug')} />
-              <Button title="ActionSheet" onPress={() => navigation.navigate('ActionSheet')} />
-              <Button title="NPS" onPress={() => navigation.navigate('SurveyNps')} />
-              <Button title="CSAT" onPress={() => navigation.navigate('SurveyCsat')} />
+            <View gap={spacing.$8}>
+              <Button
+                title="Debug"
+                onPress={() => navigation.navigate('Debug')}
+                variant="outline"
+              />
+              <Button
+                title="ActionSheet"
+                onPress={() => navigation.navigate('ActionSheet')}
+                variant="outline"
+              />
+              <Button
+                title="NPS"
+                onPress={() => navigation.navigate('SurveyNps')}
+                variant="outline"
+              />
+              <Button
+                title="CSAT"
+                onPress={() => navigation.navigate('SurveyCsat')}
+                variant="outline"
+              />
             </View>
           </Card>
           <Card>
             <Text title="Settings" size="4xl" weight="light" />
-
-            <Button title={`theme: ${theme}`} onPress={() => toggleTheme()} />
-            <Button
-              title={`user: ${user?.name}`}
-              onPress={() => {
-                Alert.alert(
-                  'Do you want to logout?',
-                  'This will clear your session and you will need to login again.',
-                  [
-                    {
-                      text: 'Cancel',
-                      style: 'cancel',
-                    },
-                    { text: 'Logout', onPress: () => logout() },
-                  ]
-                );
-              }}
-            />
+            <View gap={spacing.$8}>
+              <Button title={`theme: ${theme}`} onPress={() => toggleTheme()} variant="outline" />
+              <Button
+                title={`user: ${user?.name}`}
+                onPress={() => {
+                  Alert.alert(
+                    'Do you want to logout?',
+                    'This will clear your session and you will need to login again.',
+                    [
+                      {
+                        text: 'Cancel',
+                        style: 'cancel',
+                      },
+                      { text: 'Logout', onPress: () => logout() },
+                    ]
+                  );
+                }}
+                variant="outline"
+              />
+            </View>
           </Card>
         </View>
       </KeyboardAwareScrollView>
