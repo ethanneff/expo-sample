397bc1921aee7de7f8a663b0e80889824709af8c
Ethan Neff <ethan.neff@eneff.com>
2025-05-29 23:47:15 -0700

    add props to sign in inputs

--- src/screens/SignInScreen/SignInScreen.tsx
+++ src/screens/SignInScreen/SignInScreen.tsx
@@ -1,45 +1,94 @@
 import { useNavigation } from '@react-navigation/native';
+import { useRef, useState } from 'react';
+import { TextInput } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-controller';
 import { Button } from '~/components/Button/Button';
-import { Icon } from '~/components/Icon/Icon';
 import { Input } from '~/components/Input/Input';
 import { Pressable } from '~/components/Pressable/Pressable';
 import { Screen } from '~/components/Screen/Screen';
 import { Text } from '~/components/Text/Text';
 import { View } from '~/components/View/View';
 import { useStoreAuth } from '~/store/useStoreAuth';
-import { useAppTheme } from '~/theme/useAppTheme';
 
 const SignInScreen = () => {
-  const { spacing } = useAppTheme();
   const navigation = useNavigation();
   const login = useStoreAuth((state) => state.actions.login);
+  const emailRef = useRef<TextInput>(null);
+  const passwordRef = useRef<TextInput>(null);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignIn = () => {
+    login({ id: '1', name: 'John Doe', email: 'john.doe@example.com', token: '1234567890' });
+  };
+
   return (
     <Screen>
       <KeyboardAwareScrollView>
         <Text title="Enter your details" />
-        <Input placeholder="Email" />
-        <Input placeholder="Password" />
-        <Button
-          title="Sign In"
-          onPress={() =>
-            login({ id: '1', name: 'John Doe', email: 'john.doe@example.com', token: '1234567890' })
-          }
+        <Input
+          defaultValue={email}
+          editable={true}
+          onChangeText={setEmail}
+          submitBehavior="submit"
+          textContentType="emailAddress"
+          ref={emailRef}
+          placeholder="Email"
+          autoCapitalize="none"
+          autoComplete="email"
+          autoCorrect={false}
+          keyboardType="email-address"
+          returnKeyType="next"
+          onSubmitEditing={() => {
+            passwordRef.current?.focus();
+          }}
         />
-        <Button title="Forgot Password" onPress={() => navigation.navigate('ForgotPassword')} />
+        <Input
+          defaultValue={password}
+          editable={true}
+          onChangeText={setPassword}
+          submitBehavior="submit"
+          ref={passwordRef}
+          placeholder="Password"
+          autoCapitalize="none"
+          autoComplete="password"
+          autoCorrect={false}
+          keyboardType="visible-password"
+          secureTextEntry
+          returnKeyType="done"
+          onSubmitEditing={handleSignIn}
+          textContentType="password"
+        />
+        <Button title="Sign In" onPress={handleSignIn} variant="primary" fitContent />
+        <Button
+          title="Forgot Password"
+          onPress={() => navigation.navigate('ForgotPassword')}
+          variant="outline"
+          fitContent
+        />
 
-        <View flexDirection="row" gap={spacing.$2}>
-          <Icon name="logo-google" size={24} />
-          <Button title=" Sign in with Google" onPress={() => {}} />
-        </View>
-        <View flexDirection="row" gap={spacing.$2}>
-          <Icon name="logo-facebook" size={24} />
-          <Button title="Sign in with Facebook" onPress={() => {}} />
-        </View>
-        <View flexDirection="row" gap={spacing.$2}>
-          <Icon name="logo-apple" size={24} />
-          <Button title="Sign in with Apple" onPress={() => {}} />
-        </View>
+        <Button
+          title="Sign in with Google"
+          onPress={() => {}}
+          variant="outline"
+          icon="logo-google"
+          fitContent
+        />
+
+        <Button
+          title="Sign in with Facebook"
+          onPress={() => {}}
+          variant="outline"
+          icon="logo-facebook"
+          fitContent
+        />
+        <Button
+          title="Sign in with Apple"
+          onPress={() => {}}
+          variant="outline"
+          icon="logo-apple"
+          fitContent
+        />
         <View flexDirection="row" flexWrap="wrap">
           <Text title="By signing in to Duolingo, you agree to our " />
           <Pressable onPress={() => navigation.navigate('Terms')}>
