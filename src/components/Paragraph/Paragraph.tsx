d7db6351f4f2923ea8c4f395e785eff0d3fcd20d
Ethan Neff <ethan.neff@eneff.com>
2025-06-05 22:22:10 -0700

    add paragraph component

---
+++ src/components/Paragraph/Paragraph.tsx
@@ -0,0 +1,6 @@
+import { PropsWithChildren } from 'react';
+import { Text as RNText } from 'react-native';
+
+export const Paragraph = ({ children }: PropsWithChildren) => {
+  return <RNText>{children}</RNText>;
+};
