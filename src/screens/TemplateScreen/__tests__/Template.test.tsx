200b2a3a7fda0e4e29c77ffce00a5234cd9f587b
Ethan Neff <ethan.neff@eneff.com>
2025-06-07 23:37:26 -0700

    add tempalte tests

---
+++ src/screens/TemplateScreen/__tests__/Template.test.tsx
@@ -0,0 +1,38 @@
+describe('Template', () => {
+  describe('network', () => {
+    it('shows reconnection when lost connection', () => {});
+    it('allows user to reconnect', () => {});
+    it('shows unknown error modal', () => {});
+    it('allows user to refresh', () => {});
+  });
+
+  describe('initial load', () => {
+    it('shows loading', () => {});
+    it('shows no connection', () => {});
+    it('allows user to retry', () => {});
+    it('shows empty data', () => {});
+    it('shows data', () => {});
+  });
+
+  describe('add data', () => {
+    it('show loading', () => {});
+    it('show error when cannot create todo', () => {});
+    it('allows user to retry', () => {});
+    it('show new todo', () => {});
+  });
+
+  describe('update data', () => {
+    it('show loading', () => {});
+    it('show error when cannot update todo', () => {});
+    it('allows user to retry', () => {});
+    it('show updated todo', () => {});
+  });
+
+  describe('delete data', () => {
+    it('shows confirmation dialog', () => {});
+    it('show loading', () => {});
+    it('show error when cannot delete todo', () => {});
+    it('allows user to retry', () => {});
+    it('show deleted todo', () => {});
+  });
+});
