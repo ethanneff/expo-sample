f1cbe9be06b3d2a22ce4d8e1be2e6de84ce60e88
Ethan Neff <ethan.neff@eneff.com>
2025-06-08 23:21:55 -0700

    linting

--- src/screens/PrivacyPolicyScreen/PrivacyPolicyScreen.tsx
+++ src/screens/PrivacyPolicyScreen/PrivacyPolicyScreen.tsx
@@ -1,17 +1,16 @@
 import { useNavigation } from '@react-navigation/native';
-import { Button } from 'react-native';
+import { useCallback } from 'react';
+import { Button } from '~/components/Button/Button';
 import { Placeholder } from '~/components/Placeholder/Placeholder';
 
 const PrivacyPolicyScreen = () => {
   const navigation = useNavigation();
+  const handlePress = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
   return (
     <Placeholder title="Privacy Policy">
-      <Button
-        title="Back"
-        onPress={() => {
-          navigation.goBack();
-        }}
-      />
+      <Button onPress={handlePress} title="Back" variant="outline" />
     </Placeholder>
   );
 };
