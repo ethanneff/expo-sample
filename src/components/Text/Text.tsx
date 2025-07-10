c30d599b695bff118217b5a8c023bc6e750f56dd
Ethan Neff <ethan.neff@eneff.com>
2025-07-09 22:28:18 -0700

    refactor: relocate useTypography

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,15 +1,8 @@
 // eslint-disable-next-line no-restricted-imports
 import { Text as RNText, type TextProps, type TextStyle } from 'react-native';
+import { useTypography } from '~/components/Text/useTypography';
 import { type ColorName, useAppTheme } from '~/theme/useAppTheme';
-import { Typescript } from '~/utils/Typescript';
-import {
-  type FontFamily,
-  geistFontFamily,
-  geistMonoFontFamily,
-  letterSpacing,
-  textSize,
-  type Variant,
-} from './utilities';
+import { type FontFamily, type Variant } from './utilities';
 
 type Properties = {
   readonly accessible?: boolean;
@@ -19,6 +12,7 @@ type Properties = {
   readonly lineBreakMode?: TextProps['lineBreakMode'];
   readonly numberOfLines?: TextProps['numberOfLines'];
   readonly selectable?: boolean;
+  readonly style?: TextStyle;
   readonly textAlign?: TextStyle['textAlign'];
   readonly textDecorationColor?: ColorName;
   readonly textDecorationLine?: TextStyle['textDecorationLine'];
@@ -26,110 +20,6 @@ type Properties = {
   readonly textTransform?: TextStyle['textTransform'];
   readonly title: string;
   readonly variant?: Variant;
-};
-
-const useTypography = (variant: Variant): TextStyle => {
-  const { colors, spacing } = useAppTheme();
-  switch (variant) {
-    case 'blockquote': {
-      return {
-        borderLeftColor: colors.border,
-        borderLeftWidth: 2,
-        color: colors.mutedForeground,
-        paddingLeft: spacing.$16,
-        ...textSize.md,
-        ...geistFontFamily.regular,
-        ...letterSpacing.normal,
-      };
-    }
-    case 'code': {
-      return {
-        backgroundColor: colors.muted,
-        ...textSize.sm,
-        ...geistMonoFontFamily.semibold,
-        ...letterSpacing.normal,
-      };
-    }
-    case 'h1': {
-      return {
-        ...textSize['4xl'],
-        ...geistFontFamily.extrabold,
-        ...letterSpacing.tight,
-      };
-    }
-    case 'h2': {
-      return {
-        ...textSize['3xl'],
-        ...geistFontFamily.semibold,
-        ...letterSpacing.tight,
-        borderBottomColor: colors.border,
-        borderBottomWidth: 1.5,
-        paddingBottom: spacing.$4,
-      };
-    }
-    case 'h3': {
-      return {
-        ...textSize['2xl'],
-        ...geistFontFamily.semibold,
-        ...letterSpacing.tight,
-      };
-    }
-    case 'h4': {
-      return {
-        ...textSize.xl,
-        ...geistFontFamily.semibold,
-        ...letterSpacing.tight,
-      };
-    }
-    case 'large': {
-      return {
-        ...textSize.lg,
-        ...geistFontFamily.regular,
-        ...letterSpacing.normal,
-      };
-    }
-    case 'lead': {
-      return {
-        color: colors.mutedForeground,
-        ...textSize.xl,
-        ...geistFontFamily.regular,
-        ...letterSpacing.normal,
-      };
-    }
-    case 'muted': {
-      return {
-        color: colors.mutedForeground,
-        ...textSize.sm,
-        ...geistFontFamily.regular,
-        ...letterSpacing.normal,
-      };
-    }
-    case 'p': {
-      return {
-        ...textSize.md,
-        ...geistFontFamily.regular,
-        ...letterSpacing.normal,
-      };
-    }
-    case 'small': {
-      return {
-        ...textSize.sm,
-        ...geistFontFamily.medium,
-        ...letterSpacing.normal,
-      };
-    }
-    case 'xsmall': {
-      return {
-        color: colors.mutedForeground,
-        ...textSize.xs,
-        ...geistFontFamily.regular,
-        ...letterSpacing.normal,
-      };
-    }
-    default: {
-      return Typescript.assertNever(variant);
-    }
-  }
 };
 
 export const Text = ({
@@ -140,6 +30,7 @@ export const Text = ({
   lineBreakMode,
   numberOfLines,
   selectable = false,
+  style,
   textAlign = 'left',
   textDecorationColor,
   textDecorationLine,
@@ -167,6 +58,7 @@ export const Text = ({
         textDecorationLine,
         textDecorationStyle,
         textTransform,
+        ...style,
       }}>
       {title}
     </RNText>
